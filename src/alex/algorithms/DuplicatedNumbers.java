9c6e41a1988d4ad05abfc31840e7fe46cb320bef
Alex Soares <alex.soares@gmail.com>
2013-02-16 20:44:32 -0200

    bit operations

--- src/alex/algorithms/DuplicatedNumbers.java
+++ src/alex/algorithms/DuplicatedNumbers.java
@@ -9,17 +9,29 @@ public class DuplicatedNumbers {
 		}
 		return a;
 	}
-//	public static int findDuplicatedNumber(int[] arr){
-//		int a =0;
-//		for(int i=0; i < arr.length; i++){
-//			a=a^(arr[i]);
-//		}
-//		return a;
-//	}
-	public static void main(String[] args) {
-		int arr[] = {1,1,10,10,5,6,7,7,6};
-		System.out.printf("%d\n",findUniqueNumber(arr));
+	public static int findDuplicateNumber(int array[]){
+		int slow= array.length-1;
+		int fast = array.length-1;
+		while(true){
+			slow = array[slow];
+			fast = array[array[fast]];
+			if(slow == fast) break;
+		}
+		//  Start up another pointer from the end of the array and march it forward
+	    // until it hits the pointer inside the array.
+		int finder = array.length-1;
+		while(true){
+			slow = array[slow];
+			finder = array[finder];
+			if(slow  == finder)
+				return slow;
+		}
+	}
 
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7,8,1,9,1};
+		System.out.printf("%d\n",findUniqueNumber(arr));
+		System.out.printf("%d\n",findDuplicateNumber(arr));
 	}
 
 }
