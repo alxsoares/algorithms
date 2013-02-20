9d544758b5fd55d30fad6548763f7e73a3324628
Alex Soares <alex.soares@moip.com.br>
2013-02-20 17:10:23 -0300

    CarrotJumping

--- src/topcoder/alex/CarrotJumping.java
+++ src/topcoder/alex/CarrotJumping.java
@@ -1,8 +1,41 @@
 package topcoder.alex;
 
+/**
+ * 
+ * Problem Statement Rabbits often feel hungry, so when they go out to eat
+ * carrots, they jump as quickly as possible.
+ * 
+ * 
+ * 
+ * Initially, rabbit Hanako stands at position init. From position x, she can
+ * jump to either position 4*x+3 or 8*x+7 in a single jump. She can jump at most
+ * 100,000 times because she gets tired by jumping.
+ * 
+ * 
+ * 
+ * Carrots are planted at position x if and only if x is divisible by
+ * 1,000,000,007 (i.e. carrots are planted at position 0, position
+ * 1,000,000,007, position 2,000,000,014, and so on). Return the minimal number
+ * of jumps required to reach a carrot. If it's impossible to reach a carrot
+ * using at most 100,000 jumps, return -1.
+ * 
+ * 
+ * 
+ * 
+ * Definition
+ * 
+ * Class: CarrotJumping Method: theJump Parameters: int Returns: int Method
+ * signature: int theJump(int init) (be sure your method is public)
+ * 
+ * 
+ * Constraints - init will be between 1 and 1,000,000,006, inclusive.
+ * 
+ */
+
 public class CarrotJumping {
     long MOD = 1000000007;
 
+    // 2^n (mod MOD)
     long pow(final int v) {
         if (v == 0)
             return 1;
