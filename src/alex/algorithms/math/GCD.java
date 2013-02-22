2eb9b98dad03f4da3a49d1d6af27da1e7d496fd4
Alex Soares <alex.soares@moip.com.br>
2013-02-22 15:44:14 -0300

    math

--- src/alex/algorithms/math/GCD.java
+++ src/alex/algorithms/math/GCD.java
@@ -26,22 +26,7 @@ public class GCD {
         return v.shiftLeft(k);
     }
 
-    public static int gcd(int a, int b) {
-        while (b > 0) {
-            a = a % b;
-            System.out.println("a=>" + a);
-            System.out.println("b=>" + b);
-            a ^= b;
-            System.out.println("a=>" + a);
-            b ^= a;
-            System.out.println("b=>" + b);
-            a ^= b;
-            System.out.println("a=>" + a);
-        }
-        return a;
-    }
-
-    public static int gcdRecursive(final int u, final int v) {
+    public static int gcd(final int u, final int v) {
         if (u == v)
             return u;
         if (u == 0)
@@ -50,16 +35,16 @@ public class GCD {
             return u;
         if ((~u & 1) > 0) {// u é par
             if ((v & 1) > 0) {// v impar
-                return gcdRecursive(u >> 1, v);// v impar e u par
+                return gcd(u >> 1, v);// v impar e u par
             }
-            return gcdRecursive(u >> 1, v >> 1) << 1;// ambos são pares
+            return gcd(u >> 1, v >> 1) << 1;// ambos são pares
         }
         if ((~v & 1) > 0)
-            return gcdRecursive(u, v >> 1);// v par e u impar
+            return gcd(u, v >> 1);// v par e u impar
         if (u > v) {
-            return gcdRecursive((u - v) >> 1, v);
+            return gcd((u - v) >> 1, v);
         } else {
-            return gcdRecursive(u, (v - u) >> 1);
+            return gcd(u, (v - u) >> 1);
         }
     }
 
@@ -73,14 +58,8 @@ public class GCD {
      * @param args
      */
     public static void main(final String[] args) {
-        long begin = System.currentTimeMillis();
+        System.out.println(gcd(784777743, 928392839));
         System.out.println(gcd(943094, 343434));
-        long end = System.currentTimeMillis();
-        System.out.println("Tempo=>" + (end - begin));
-        begin = System.currentTimeMillis();
-        System.out.println(gcdRecursive(943094, 343434));
-        end = System.currentTimeMillis();
-        System.out.println("Tempo=>" + (end - begin));
         // System.out.println(lcm(3, 6));
         // System.out.println(bitwiseGcd(new BigInteger("94839048390483944"),
         // new BigInteger("893748937489374")));
--- src/alex/algorithms/math/Math.java
+++ src/alex/algorithms/math/Math.java
@@ -3,7 +3,7 @@ package alex.algorithms.math;
 public class Math {
     private static final double EPSLON = Double.MIN_VALUE;
 
-    public static void IntegerDivision(int numerator, int denominator) {
+    public static void IntegerDivision(int numerator, final int denominator) {
         int quot = 0;
         while (numerator > denominator) {
             numerator = numerator - denominator;
@@ -11,11 +11,10 @@ public class Math {
         }
         int rest = numerator;
 
-        System.out.println("Quotient=[" + quot + "] and Denominator=[" + rest
-                + "]");
+        System.out.println("Quotient=[" + quot + "] and Denominator=[" + rest + "]");
     }
 
-    public static double sqrt(double n) {
+    public static double sqrt(final double n) {
         double x0 = 6 * 100000;
         double variation = Double.MAX_VALUE;
         while (variation > EPSLON) {
@@ -27,7 +26,7 @@ public class Math {
         return x0;
     }
 
-    public static double pow2(double n, int e) {
+    public static double pow2(final double n, final int e) {
         if (e == 0)
             return 1;
         double r = n;
@@ -37,7 +36,7 @@ public class Math {
         return r;
     }
 
-    public static double pow(double n, int e) {
+    public static double pow(final double n, int e) {
         int one = 0x01;
         double factor = n;
         double res = 1;
@@ -46,12 +45,12 @@ public class Math {
                 res = res * factor;
             }
             e = e >> 1;
-            factor = factor*factor;
+            factor = factor * factor;
         }
         return res;
     }
 
-    public static double pow(double n, double e) {
+    public static double pow(final double n, final double e) {
         double decExp = e - (int) e;
         double partialExp = 0.5;
         double res = 1;
@@ -68,10 +67,35 @@ public class Math {
         return res * pow(n, (int) e);
     }
 
-    public static void main(String[] args) {
-        IntegerDivision(200, 7);
-        System.out.println(sqrt(36.0));
-        System.out.println(pow(2, 0.5));
-        System.out.println(pow2(2,3));
+    public static void primes(final int n) {
+        int[] p = new int[n + 1];
+        p[2] = 0;
+        for (int i = 4; i <= n; i += 2) {
+            p[i]++;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (p[i] == 0) {
+                for (int j = i * 2; j <= n; j += i) {
+                    p[j] = p[j] + 1;
+                }
+            }
+        }
+        for (int i = 2; i < p.length; i++) {
+            int j = p[i];
+            if (j == 0) {
+                System.out.printf("%d is prime\n", i);
+            }
+            // else {
+            // System.out.printf("%d tem %d divisores\n", i, j);
+            // }
+        }
+    }
+
+    public static void main(final String[] args) {
+        // IntegerDivision(200, 7);
+        // System.out.println(sqrt(36.0));
+        // System.out.println(pow(2, 0.5));
+        // System.out.println(pow2(2, 3));
+        primes(20000);
     }
 }
