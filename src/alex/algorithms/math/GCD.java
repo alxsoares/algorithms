21b19f5537eb000d9a852efd7acc8fb3c85c26bf
Alex Soares <alex.soares@gmail.com>
2013-02-24 15:06:16 -0300

    math gcd

--- src/alex/algorithms/math/GCD.java
+++ src/alex/algorithms/math/GCD.java
@@ -3,66 +3,86 @@ package alex.algorithms.math;
 import java.math.BigInteger;
 
 public class GCD {
-    public static BigInteger bitwiseGcd(BigInteger u, BigInteger v) {
-        if (u.equals(BigInteger.ZERO))
-            return v;
-        if (v.equals(BigInteger.ZERO))
-            return u;
+	public static BigInteger bitwiseGcd(BigInteger u, BigInteger v) {
+		if (u.equals(BigInteger.ZERO))
+			return v;
+		if (v.equals(BigInteger.ZERO))
+			return u;
 
-        int uBit = u.getLowestSetBit();
-        int vBit = v.getLowestSetBit();
-        int k = (uBit <= vBit ? uBit : vBit);
+		int uBit = u.getLowestSetBit();
+		int vBit = v.getLowestSetBit();
+		int k = (uBit <= vBit ? uBit : vBit);
 
-        while (u.signum() > 0) {
-            u = u.shiftRight(u.getLowestSetBit());
-            v = v.shiftRight(v.getLowestSetBit());
-            if (u.subtract(v).signum() >= 0) {
-                u = (u.subtract(v)).shiftRight(1);
-            } else {
-                v = (v.subtract(u)).shiftRight(1);
-            }
-        }
+		while (u.signum() > 0) {
+			u = u.shiftRight(u.getLowestSetBit());
+			v = v.shiftRight(v.getLowestSetBit());
+			if (u.subtract(v).signum() >= 0) {
+				u = (u.subtract(v)).shiftRight(1);
+			} else {
+				v = (v.subtract(u)).shiftRight(1);
+			}
+		}
 
-        return v.shiftLeft(k);
-    }
+		return v.shiftLeft(k);
+	}
 
-    public static int gcd(final int u, final int v) {
-        if (u == v)
-            return u;
-        if (u == 0)
-            return v;
-        if (v == 0)
-            return u;
-        if ((~u & 1) > 0) {// u é par
-            if ((v & 1) > 0) {// v impar
-                return gcd(u >> 1, v);// v impar e u par
-            }
-            return gcd(u >> 1, v >> 1) << 1;// ambos são pares
-        }
-        if ((~v & 1) > 0)
-            return gcd(u, v >> 1);// v par e u impar
-        if (u > v) {
-            return gcd((u - v) >> 1, v);
-        } else {
-            return gcd(u, (v - u) >> 1);
-        }
-    }
+	public static int gcd(final int u, final int v) {
+		if (u == v)
+			return u;
+		if (u == 0)
+			return v;
+		if (v == 0)
+			return u;
+		if ((~u & 1) > 0) {// u é par
+			if ((v & 1) > 0) {// v impar
+				return gcd(u >> 1, v);// v impar e u par
+			}
+			return gcd(u >> 1, v >> 1) << 1;// ambos são pares
+		}
+		if ((~v & 1) > 0)
+			return gcd(u, v >> 1);// v par e u impar
+		if (u > v) {
+			return gcd((u - v) >> 1, v);
+		} else {
+			return gcd(u, (v - u) >> 1);
+		}
+	}
 
-    public static int lcm(final int a, final int b) {
-        return a * b / gcd(a, b);
-    }
+	public static int gcdAlex(final int u, final int v) {
+		if (u == v)
+			return u;
+		if (u == 0 || v == 0)
+			return java.lang.Math.max(u, v);
+		if (u % 2 == 0) {
+			if (v % 2 == 0) {
+				return gcdAlex(u >> 1, v >> 1) << 1;
+			}
+			return gcdAlex(u >> 1, v);
+		}
+		if (v % 2 == 0) {
+			return gcdAlex(u, v >> 1);
+		}
+		if (u > v)
+			return gcdAlex((u - v) >> 1, v);
+		return gcdAlex(u, (v - u) >> 1);
+	}
 
-    /**
-     * TODO: fazer iterativo
-     * 
-     * @param args
-     */
-    public static void main(final String[] args) {
-        System.out.println(gcd(784777743, 928392839));
-        System.out.println(gcd(943094, 343434));
-        // System.out.println(lcm(3, 6));
-        // System.out.println(bitwiseGcd(new BigInteger("94839048390483944"),
-        // new BigInteger("893748937489374")));
-    }
+	public static int lcm(final int a, final int b) {
+		return a * b / gcd(a, b);
+	}
+
+	/**
+	 * TODO: fazer iterativo
+	 * 
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		System.out.println(gcd(784777743, 928392839));
+		System.out.println(gcd(943094, 343434));
+		System.out.println(gcdAlex(1024, 7*1024));
+		// System.out.println(lcm(3, 6));
+		// System.out.println(bitwiseGcd(new BigInteger("94839048390483944"),
+		// new BigInteger("893748937489374")));
+	}
 
 }
--- src/alex/algorithms/math/Math.java
+++ src/alex/algorithms/math/Math.java
@@ -1,101 +1,127 @@
 package alex.algorithms.math;
 
 public class Math {
-    private static final double EPSLON = Double.MIN_VALUE;
+	private static final double EPSLON = Double.MIN_VALUE;
 
-    public static void IntegerDivision(int numerator, final int denominator) {
-        int quot = 0;
-        while (numerator > denominator) {
-            numerator = numerator - denominator;
-            quot++;
-        }
-        int rest = numerator;
+	public static void IntegerDivision(int numerator, final int denominator) {
+		int quot = 0;
+		while (numerator > denominator) {
+			numerator = numerator - denominator;
+			quot++;
+		}
+		int rest = numerator;
 
-        System.out.println("Quotient=[" + quot + "] and Denominator=[" + rest + "]");
-    }
+		System.out.println("Quotient=[" + quot + "] and Denominator=[" + rest
+				+ "]");
+	}
 
-    public static double sqrt(final double n) {
-        double x0 = 6 * 100000;
-        double variation = Double.MAX_VALUE;
-        while (variation > EPSLON) {
-            double newX0 = 0.5 * (x0 + n / x0);
-            variation = x0 - newX0;
-            x0 = newX0;
-        }
-        System.out.println("Variation = [" + variation + "]");
-        return x0;
-    }
+	public static double sqrt(final double n) {
+		double x0 = 6 * 100000;
+		double variation = Double.MAX_VALUE;
+		while (variation > EPSLON) {
+			double newX0 = 0.5 * (x0 + n / x0);
+			variation = x0 - newX0;
+			x0 = newX0;
+		}
+		System.out.println("Variation = [" + variation + "]");
+		return x0;
+	}
 
-    public static double pow2(final double n, final int e) {
-        if (e == 0)
-            return 1;
-        double r = n;
-        for (int i = 1; i < e; i++) {
-            r *= n;
-        }
-        return r;
-    }
+	public static double pow2(final double n, final int e) {
+		if (e == 0)
+			return 1;
+		double r = n;
+		for (int i = 1; i < e; i++) {
+			r *= n;
+		}
+		return r;
+	}
 
-    public static double pow(final double n, int e) {
-        int one = 0x01;
-        double factor = n;
-        double res = 1;
-        while (e > 0) {
-            if ((e & one) == 1) {
-                res = res * factor;
-            }
-            e = e >> 1;
-            factor = factor * factor;
-        }
-        return res;
-    }
+	public static double pow(final double n, int e) {
+		int one = 0x01;
+		double factor = n;
+		double res = 1;
+		while (e > 0) {
+			if ((e & one) == 1) {
+				res = res * factor;
+			}
+			e = e >> 1;
+			factor = factor * factor;
+		}
+		return res;
+	}
 
-    public static double pow(final double n, final double e) {
-        double decExp = e - (int) e;
-        double partialExp = 0.5;
-        double res = 1;
-        double factor = sqrt(n);
-        while (partialExp > Math.EPSLON) {
-            if (decExp >= partialExp) {
-                res = res * factor;
-                decExp -= partialExp;
-            }
-            partialExp *= 0.5;
-            factor = sqrt(factor);
-        }
+	public static double pow(final double n, final double e) {
+		double decExp = e - (int) e;
+		double partialExp = 0.5;
+		double res = 1;
+		double factor = sqrt(n);
+		while (partialExp > Math.EPSLON) {
+			if (decExp >= partialExp) {
+				res = res * factor;
+				decExp -= partialExp;
+			}
+			partialExp *= 0.5;
+			factor = sqrt(factor);
+		}
 
-        return res * pow(n, (int) e);
-    }
+		return res * pow(n, (int) e);
+	}
 
-    public static void primes(final int n) {
-        int[] p = new int[n + 1];
-        p[2] = 0;
-        for (int i = 4; i <= n; i += 2) {
-            p[i]++;
-        }
-        for (int i = 3; i * i <= n; i += 2) {
-            if (p[i] == 0) {
-                for (int j = i * 2; j <= n; j += i) {
-                    p[j] = p[j] + 1;
-                }
-            }
-        }
-        for (int i = 2; i < p.length; i++) {
-            int j = p[i];
-            if (j == 0) {
-                System.out.printf("%d is prime\n", i);
-            }
-            // else {
-            // System.out.printf("%d tem %d divisores\n", i, j);
-            // }
-        }
-    }
+	public static void primes(final int n) {
+		int[] p = new int[n + 1];
+		p[2] = 0;
+		for (int i = 4; i <= n; i += 2) {
+			p[i]++;
+		}
+		for (int i = 3; i * i <= n; i += 2) {
+			if (p[i] == 0) {
+				for (int j = i * 2; j <= n; j += i) {
+					p[j] = p[j] + 1;
+				}
+			}
+		}
+		for (int i = 2; i < p.length; i++) {
+			int j = p[i];
+			if (j == 0) {
+				System.out.printf("%d is prime\n", i);
+			}
+			// else {
+			// System.out.printf("%d tem %d divisores\n", i, j);
+			// }
+		}
+	}
 
-    public static void main(final String[] args) {
-        // IntegerDivision(200, 7);
-        // System.out.println(sqrt(36.0));
-        // System.out.println(pow(2, 0.5));
-        // System.out.println(pow2(2, 3));
-        primes(20000);
-    }
+	public static int trailingZeroes(int num) {
+		int r = 0;
+		if (num < 5)
+			return r;
+		while (num > 0) {
+			r += num / 5;
+			num /= 5;
+		}
+		return r;
+	}
+
+	public static int numZeros(int num) {
+		int count = 0;
+		if (num < 0) {
+			System.out.println("Factorial is not defined for < 0");
+			return 0;
+		}
+		for (int i = 5; num / i > 0; i *= 5) {
+			count += num / i;
+		}
+		return count;
+	}
+
+	public static void main(final String[] args) {
+		// IntegerDivision(200, 7);
+		// System.out.println(sqrt(36.0));
+		// System.out.println(pow(2, 0.5));
+		// System.out.println(pow2(2, 3));
+		primes(20000);
+		System.out.println(trailingZeroes(200));
+		System.out.println(numZeros(200));
+	}
 }
