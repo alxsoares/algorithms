198177b1217dd8b3d2bb2a3fc3738e830b8d2c69
Alex Soares <alex.soares@gmail.com>
2013-02-26 01:27:24 -0300

    Coin Change

--- src/alex/algorithms/dynamicprogramming/CountingChange.java
+++ src/alex/algorithms/dynamicprogramming/CountingChange.java
@@ -3,13 +3,12 @@ package alex.algorithms.dynamicprogramming;
 public class CountingChange {
 	private static int MAXTOTAL = 10000;
 	static long nway[] = new long[MAXTOTAL + 1];
-	static int coin[] = { 50, 25, 10, 5, 1 };
+	static int coin[] = { 50, 25, 10, 5,2, 1 };
 
 	public static void main(String[] args) {
-		int v = 5;
-		int n=5;
+		int n=4;
 		nway[0] = 1;
-		for (int i = 0; i < v; i++) {
+		for (int i = 0; i < coin.length; i++) {
 			int c = coin[i];
 			for (int j = c; j <= n; j++)
 				nway[j] += nway[j - c];
