54bd3e2e293544906336194f7d8747c20cc4ef75
Alex Soares <alex.soares@gmail.com>
2013-02-26 23:06:14 -0300

    kadane

--- src/alex/algorithms/Kadane.java
+++ src/alex/algorithms/Kadane.java
@@ -1,34 +1,59 @@
 package alex.algorithms;
 
 public class Kadane {
-	
-	public static int kadane(int[] numbers){
-		int begin =0;
-		int beginTemp=0;
-		int max=numbers[0];
+
+	public static int kadane(int[] numbers) {
+		int begin = 0;
+		int beginTemp = 0;
+		int max = numbers[0];
 		int maxTemp = numbers[0];
-		int end=0;
-		for(int i=1; i < numbers.length; i++){
-			if(maxTemp < 0){
+		int end = 0;
+		for (int i = 1; i < numbers.length; i++) {
+			if (maxTemp < 0) {
 				maxTemp = numbers[i];
-				beginTemp =i;
-			}else{
-				maxTemp+=numbers[i];
+				beginTemp = i;
+			} else {
+				maxTemp += numbers[i];
 			}
-			
-			if(maxTemp > max){
+
+			if (maxTemp > max) {
 				max = maxTemp;
 				begin = beginTemp;
-				end=i;
-				
+				end = i;
+
 			}
 		}
 		System.out.printf("%d %d\n", begin, end);
 		return max;
 	}
-	
+
+	public static int kadane2(int[] numbers) {
+		int begin = 0;
+		int end = 0;
+		int beginAux = numbers[0];
+		int max = numbers[0];
+		int maxTemp = numbers[0];
+		for (int i = 1; i < numbers.length; i++) {
+			if (maxTemp < 0) {
+				maxTemp = numbers[i];
+				beginAux = i;
+			} else {
+				maxTemp += numbers[i];
+			}
+			if (maxTemp > max) {
+				max = maxTemp;
+				begin = beginAux;
+				end = i;
+			}
+		}
+		System.out.printf("%d %d\n", begin, end);
+		return max;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(kadane(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+		System.out.println(kadane(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 8 }));
+		System.out
+				.println(kadane2(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 8 }));
 	}
 
 }
