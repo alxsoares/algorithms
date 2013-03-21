1e07779d50886911f1385c2507f25360d4d518a1
Alex Soares <alex.soares@moip.com.br>
2013-03-21 19:01:57 -0300

    Prim with Priority Queue

--- src/alex/algorithms/graphs/PrimMST.java
+++ src/alex/algorithms/graphs/PrimMST.java
@@ -1,6 +1,54 @@
 package alex.algorithms.graphs;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
 public class PrimMST {
+
+    public void primMST(final List<ArrayList<Integer>> G, final int W[][]) {
+
+        int V = G.size();
+        int parent[] = new int[V];
+        final Integer key[] = new Integer[V];
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(G.size(), new Comparator<Integer>() {
+            @Override
+            public int compare(final Integer o1, final Integer o2) {
+                return key[o1].compareTo(key[o2]);
+            }
+        });
+
+        for (int v = 1; v < V; ++v) {
+            parent[v] = -1;
+            key[v] = Integer.MAX_VALUE;
+
+        }
+
+        key[0] = 0;
+        minHeap.add(0);
+
+        while (!minHeap.isEmpty()) {
+            int u = minHeap.remove();
+            List<Integer> adj = G.get(u);
+            for (Integer v : adj) {
+                if (minHeap.contains(v) && W[u][v] < key[v]) {
+                    key[v] = W[u][v];
+                    parent[v] = u;
+                    minHeap.remove(v);
+                    minHeap.add(v);
+                }
+            }
+        }
+        printArr(parent);
+    }
+
+    void printArr(final int arr[]) {
+        for (int i = 1; i < arr.length; ++i)
+            System.out.printf("%d - %d\n", arr[i], i);
+    }
 
     public void primMST(final int G[][]) {
 
@@ -65,5 +113,27 @@ public class PrimMST {
       //@formatter:on
         PrimMST p = new PrimMST();
         p.primMST(G);
+
+        Random rand = new Random(System.currentTimeMillis());
+
+        ArrayList<ArrayList<Integer>> GL = new ArrayList<ArrayList<Integer>>();
+        int[][] W = new int[100][100];
+        for (int i = 0; i < 100; i++) {
+            GL.add(new ArrayList<Integer>());
+            for (int j = 0; j < 100; j++) {
+                if (i == 0 && j == 99) {
+                    GL.get(i).add(j, 1);
+                } else if (i == j) {
+                    GL.get(i).add(j, 0);
+                } else if (j < i) {
+                    GL.get(i).add(j, GL.get(j).get(i));
+                } else {
+                    int w = (Math.abs(rand.nextInt()) % 50) + 10;
+                    GL.get(i).add(j, w);
+                    W[i][j] = w;
+                }
+            }
+        }
+        p.primMST(GL, W);
     }
 }
