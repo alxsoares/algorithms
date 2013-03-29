da102a87bc851d32dc880c77d951ddece0c94883
Alex Soares <alex.soares@gmail.com>
2013-03-29 18:02:50 -0300

    Array's problems

--- src/topcoder/alex/misc/ArraysProblems.java
+++ src/topcoder/alex/misc/ArraysProblems.java
@@ -8,28 +8,80 @@ public class ArraysProblems {
 	 * Given an unsorted array of nonnegative integers, find a continous
 	 * subarray which adds to a given number.
 	 */
-	public static void subArraySum(int [] a, int sum) {
-		int start =0;
+	public static void subArraySum(int[] a, int sum) {
+		int start = 0;
 		int currSum = a[0];
-		for(int i=1; i < a.length;i++){
-			while(currSum > sum && start < i-1){
-				currSum-=a[start++];
+		for (int i = 1; i < a.length; i++) {
+			while (currSum > sum && start < i - 1) {
+				currSum -= a[start++];
 			}
-			if(currSum == sum){
-				System.out.printf("start = %d end = %d", start,i-1);
+			if (currSum == sum) {
+				System.out.printf("start = %d end = %d\n", start, i - 1);
 				return;
 			}
-			currSum+=a[i];
+			currSum += a[i];
 		}
-		System.out.println("Subarray not found");
+		System.out.println("Subarray not found\n");
+	}
+
+	/**
+	 * Given an unsorted array arr[0..n-1] of size n, find the minimum length
+	 * subarray arr[s..e] such that sorting this subarray makes the whole array
+	 * sorted.
+	 * 
+	 */
+	public static void printMinimumUnsorted(int[] a) {
+		int start = 0;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] > a[i + 1]) {
+				start = i;
+				break;
+			}
+		}
+		if (start == a.length - 1) {
+			System.out.println("Array is sorted");
+			return;
+		}
+		int end = a.length - 1;
+		for (int i = a.length - 1; i > 0; i--) {
+			if (a[i] < a[i - 1]) {
+				end = i;
+				break;
+			}
+		}
+		int max = a[start];
+		int min = a[start];
+		for (int i = start + 1; i <= end; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+		for(int i=0; i < start;i++){
+			if(a[i] > min){
+				start = i;
+				break;
+			}
+		}
+		for(int i=a.length-1; i> end;i--){
+			if(a[i] < max){
+				end = i;
+				break;
+			}
+		}
+		System.out.printf("Minimum array to be sorted %d %d", start, end);
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int arr[] = {15, 2, 4, 8, 9, 5, 10, 23};
+		int arr[] = { 15, 2, 4, 8, 9, 5, 10, 23 };
 		subArraySum(arr, 23);
+		int a[] = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+		printMinimumUnsorted(a);
 	}
 
 }
