669c4b136eab233267f0b6fcf68af585b29d688b
Alex Soares <alex.soares@gmail.com>
2013-03-31 20:01:09 -0300

    Knuth Morris Pratt

---
+++ src/alex/algorithms/pattern/KMP.java
@@ -0,0 +1,61 @@
+package alex.algorithms.pattern;
+
+public class KMP {
+	/**
+	 * Computes Longest Prefix Suffix 
+	 */
+	public static int[] computeLPS(char[] pattern) {
+
+		int[] lps = new int[pattern.length];
+		int previouslength = 0;
+		int i = 0;
+		lps[0] = 0;
+		i = 1;
+
+		while (i < pattern.length) {
+			if (pattern[i] == pattern[previouslength]) {
+				previouslength++;
+				lps[i++] = previouslength;
+			} else {
+				if (previouslength != 0) {
+					previouslength = lps[previouslength - 1];
+				} else {
+					lps[i] = 0;
+					i++;
+				}
+			}
+
+		}
+
+		return lps;
+	}
+
+	public static void KMPSearch(String text, String pattern) {
+		int[] lps = computeLPS(pattern.toCharArray());
+		int i = 0, j = 0;
+		while (i < text.length()) {
+			if (pattern.charAt(j) == text.charAt(i)) {
+				i++;
+				j++;
+			}
+			if (j == pattern.length()) {
+				System.out.printf("Pattern found at %d\n", i - j);
+				j = lps[j - 1];
+			} else if ( pattern.charAt(j) != text.charAt(i)) {
+				if (j != 0) {
+					j = lps[j - 1];
+				} else {
+					i++;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String text = "ABABDABACDABABCABAB";
+		String pattern = "ABABCABAB";
+		KMPSearch(text,pattern);
+
+	}
+
+}
