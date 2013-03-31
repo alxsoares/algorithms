d7710a10c92d9b1e3d8a9e0d47342bf30315992e
Alex Soares <alex.soares@gmail.com>
2013-03-30 23:14:23 -0300

    Pattern Search using Automata Algorithm

--- src/alex/algorithms/pattern/search/FiniteAutomata.java
+++ src/alex/algorithms/pattern/search/FiniteAutomata.java
@@ -24,21 +24,23 @@ public class FiniteAutomata {
 		return 0;
 	}
 
-	public static void computeTransitionsTable(char[] pattern, int M, int[][] TF) {
+	public static void computeTransitionsFunctionsNaive(char[] pattern, int M,
+			int[][] TF) {
 		for (int state = 0; state <= M; state++) {
 			for (int x = 0; x < NUMBER_OF_CHARS; x++) {
 				TF[state][x] = nextState(pattern, M, state, x);
 			}
 		}
 	}
+
 	/**
-	 * Complexity O(m^3*NUMBER_OF_CHARS)-> to be improved 
+	 * Time Complexity O((m^3)*NUMBER_OF_CHARS)-> to be improved
 	 */
 	public static void search(String pattern, String text) {
 		int M = pattern.length();
 		int N = text.length();
 		int[][] TF = new int[M + 1][NUMBER_OF_CHARS];
-		computeTransitionsTable(pattern.toCharArray(), M, TF);
+		computeTransitionsFunctionsNaive(pattern.toCharArray(), M, TF);
 		int state = 0;
 		for (int i = 0; i < N; i++) {
 			state = TF[state][text.charAt(i)];
@@ -48,8 +50,41 @@ public class FiniteAutomata {
 		}
 	}
 
+	public static int[][] computeTransitionsFunctions(char[] pattern, int M) {
+		int[][] TF = new int[M + 1][NUMBER_OF_CHARS];
+
+		for (int x = 0; x < NUMBER_OF_CHARS; x++) {
+			TF[0][x] = 0;
+		}
+		TF[0][pattern[0]] = 1;
+		int lps = 0;
+		for (int i = 1; i <= M; i++) {
+			for (int x = 0; x < NUMBER_OF_CHARS; x++) {
+				TF[i][x] = TF[lps][x];
+			}
+			if (i < M)
+			{
+				TF[i][pattern[i]] = i + 1;
+				lps = TF[lps][pattern[i]];
+			}
+		}
+		return TF;
+	}
+	public static void searchOptimised(String pattern, String text) {
+		int M = pattern.length();
+		int N = text.length();
+		int[][] TF = computeTransitionsFunctions(pattern.toCharArray(), M);
+		int state = 0;
+		for (int i = 0; i < N; i++) {
+			state = TF[state][text.charAt(i)];
+			if (state == M) {
+				System.out.printf("Pattern found at index %d\n", i - M + 1);
+			}
+		}
+	}
 	public static void main(String[] args) {
 		search("Alex", "AlexAlexAlexAAAAAAAAAaaaaAlex");
+		searchOptimised("Alex", "AlexAlexAlexAAAAAAAAAaaaaAlex");
 	}
 
 }
