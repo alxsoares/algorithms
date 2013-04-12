ceb5e4a84c55097ecaa6df754c729845a4bc1891
Alex Soares <alex.soares@gmail.com>
2013-04-12 00:11:27 -0300

    first regexp

--- src/alex/algorithms/strings/Regexp.java
+++ src/alex/algorithms/strings/Regexp.java
@@ -32,22 +32,9 @@ public class Regexp {
 		return false;
 	}
 
-	public static void test(String first, String second) {
-		if (match(first, second)) {
-			System.out.println("Yes");
-		} else
-			System.out.println("No");
-	}
 
 	public static void main(String[] args) {
-		 test("g*ks", "geeks"); // Yes
-		    test("ge?ks*", "geeksforgeeks"); // Yes
-		    test("g*k", "gee");  // No because 'k' is not in second
-		    test("*pqrs", "pqrst"); // No because 't' is not in first
-		    test("abc*bcd", "abcdhghgbcd"); // Yes
-		    test("abc*c?d", "abcd"); // No because second must have 2 instances of 'c'
-		    test("*c*d", "abcd"); // Yes
-		    test("*?c*d", "abcd"); // Yes
+		 
 	}
 
 }
