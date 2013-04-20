62fbd933ca673a4706408caee23374be1a458d06
Alex Soares <alex.soares@gmail.com>
2013-04-19 22:35:56 -0300

    Maximum rectangular area in a histogram

--- src/topcoder/alex/misc/MaxRectangleHistogram.java
+++ src/topcoder/alex/misc/MaxRectangleHistogram.java
@@ -1,5 +1,7 @@
 package topcoder.alex.misc;
 
+import java.util.Stack;
+
 public class MaxRectangleHistogram {
 
 	public static int findMinimum(int[] hist, int start, int end) {
@@ -29,9 +31,39 @@ public class MaxRectangleHistogram {
 		return getMaxAreaRectangle(hist, 0, hist.length - 1);
 	}
 
+	// O(n) complexity time
+	public static int getMaxArea2(int hist[]) {
+		Stack<Integer> s = new Stack<Integer>();
+		int maxArea = 0;
+		int areaWithTop;
+		int i = 0;
+		while (i < hist.length) {
+			if (s.isEmpty() || hist[s.peek()] <= hist[i]) {
+				s.push(i++);
+			} else {
+				int index = s.pop();
+				areaWithTop = hist[index] * (s.empty() ? i : i - s.peek() - 1);
+				if (areaWithTop > maxArea) {
+					maxArea = areaWithTop;
+				}
+			}
+		}
+		while(!s.isEmpty()){
+			int index = s.pop();
+			areaWithTop = hist[index] * (s.empty() ? i : i - s.peek() - 1);
+			if (areaWithTop > maxArea) {
+				maxArea = areaWithTop;
+			}
+		}
+
+		return maxArea;
+	}
+
 	public static void main(String[] args) {
 		int hist[] = { 6, 1, 5, 4, 5, 2, 6 };
 		System.out.printf("Max area is %d\n", getMaxArea(hist));
+		System.out.printf("Max area is %d\n", getMaxArea2(hist));
+		System.out.printf("Max area is %d\n", getMaxArea2(new int[]{6, 2, 5, 4, 5, 1, 6}));
 	}
 
 }
