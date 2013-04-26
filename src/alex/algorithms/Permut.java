ae51a62f3608910a26ef3168770a83b9dc35f282
Alex Soares <alex.soares@moip.com.br>
2013-04-26 18:16:29 -0300

    Simulated Annealing

--- src/alex/algorithms/Permut.java
+++ src/alex/algorithms/Permut.java
@@ -1,69 +1,69 @@
 package alex.algorithms;
 
 public class Permut {
-	public void permut(char src[], char dest[], int pos, int size) {
-		if (pos < size) {
-			for (int i = 0; i < src.length; i++) {
-				dest[pos] = src[i];
-				permut(src, dest, pos + 1, size);
-			}
-		} else {
-			System.out.println(dest);
-		}
-	}
+    public void permut(final char src[], final char dest[], final int pos, final int size) {
+        if (pos < size) {
+            for (int i = 0; i < src.length; i++) {
+                dest[pos] = src[i];
+                permut(src, dest, pos + 1, size);
+            }
+        } else {
+            System.out.println(dest);
+        }
+    }
 
-	public void permutString(char str[], int n) {
-		final int len = str.length;
-		if (n == len - 2) {
-			System.out.println(str);
-			char aux = str[len - 1];
-			str[len - 1] = str[len - 2];
-			str[len - 2] = aux;
-			System.out.println(str);
-			str[len - 2] = str[len - 1];
-			str[len - 1] = aux;
-		} else {
-			for (int i = n; i < str.length; i++) {
-				char aux = str[i];
-				str[i] = str[n];
-				str[n] = aux;
-				permutString(str, n + 1);
-				str[n] = str[i];
-				str[i] = aux;
-			}
-		}
-	}
+    public void permutString(final char str[], final int n) {
+        final int len = str.length;
+        if (n == len - 2) {
+            System.out.println(str);
+            char aux = str[len - 1];
+            str[len - 1] = str[len - 2];
+            str[len - 2] = aux;
+            System.out.println(str);
+            str[len - 2] = str[len - 1];
+            str[len - 1] = aux;
+        } else {
+            for (int i = n; i < str.length; i++) {
+                char aux = str[i];
+                str[i] = str[n];
+                str[n] = aux;
+                permutString(str, n + 1);
+                str[n] = str[i];
+                str[i] = aux;
+            }
+        }
+    }
 
-	void print(char[] s, int[] perm, int n) {
-		for (int i = 0; i < n; i++)
-			System.out.printf("%c", s[perm[i]]);
-		System.out.printf("\n");
-	}
+    void print(final char[] s, final int[] perm, final int n) {
+        for (int i = 0; i < n; i++)
+            System.out.printf("%c", s[perm[i]]);
+        System.out.printf("\n");
+    }
 
-	void perm(char[] s, int[] stack, int n, int level) {
-		if (level == n) {
-			print(s, stack, n);
-			return;
-		}
-		for (int i = 0; i < n; i++) {
-			stack[level] = i;
-			boolean discard = false;
-			for (int j = 0; j < level; j++) {
-				if (stack[j] == i) {
-					discard = true;
-					break;
-				}
-			}
-			if (!discard)
-				perm(s, stack, n, level + 1);
-		}
-	}
+    void perm(final char[] s, final int[] stack, final int n, final int level) {
+        if (level == n) {
+            print(s, stack, n);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            stack[level] = i;
+            boolean discard = false;
+            for (int j = 0; j < level; j++) {
+                if (stack[j] == i) {
+                    discard = true;
+                    break;
+                }
+            }
+            if (!discard)
+                perm(s, stack, n, level + 1);
+        }
+    }
 
-	public static void main(String[] args) {
-		Permut permut = new Permut();
-		String src = "0123456";
-		// permut.permut(src.toCharArray(),new char[7],0,7);
-		// permut.permutString(src.toCharArray(),0);
-		permut.perm(src.toCharArray(), new int[src.length()], src.length(), 0);
-	}
+    public static void main(final String[] args) {
+        Permut permut = new Permut();
+        String src = "0123456789ABC";
+        // permut.permut(src.toCharArray(),new char[7],0,7);
+        // permut.permutString(src.toCharArray(),0);
+        permut.perm(src.toCharArray(), new int[src.length()], src.length(), 0);
+    }
 }
