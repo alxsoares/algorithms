9df9180f86ce8d4054905b6351e85d8c1666f10e
Alex Soares <alex.soares@gmail.com>
2013-04-28 13:44:48 -0300

    Regular Expression

--- src/alex/algorithms/strings/Regexp.java
+++ src/alex/algorithms/strings/Regexp.java
@@ -14,14 +14,13 @@ public class Regexp {
 		// Make sure that the characters after '*' are present in second string.
 		// This function assumes that the first string will not contain two
 		// consecutive '*'
-		if (first.charAt(0) == '*' && first.length()==1
-				&& "".equals(second))
+		if (first.charAt(0) == '*' && first.length() == 1 && "".equals(second))
 			return false;
 
 		// If the first string contains '?' or the current characters of
 		// both strings match
 		if (first.charAt(0) == '?' || first.charAt(0) == second.charAt(0))
-				return match(first.substring(1), second.substring(1));
+			return match(first.substring(1), second.substring(1));
 
 		// If there is *, then there are two possibilities
 		// a) We consider current character of second string
@@ -32,9 +31,41 @@ public class Regexp {
 		return false;
 	}
 
+	public static boolean matchRegexp(String string, String pattern) {
+		if (string == null || pattern == null)
+			return false;
+		return matchCore(string, pattern);
+	}
+
+	private static boolean matchCore(String string, String pattern) {
+		if (string.length()==0 && pattern.length()==0)
+			return true;
+		if (string.length()!=0 && pattern.length()==0)
+			return true;
+
+		if (pattern.length()>=2 && pattern.charAt(1) == '*') {
+			if (pattern.charAt(0) == string.charAt(0)
+					|| (pattern.charAt(0) == '.' && string.length()!=0))
+				// move on the next state
+				return matchCore(string.substring(1), pattern.substring(2))
+				// stay on the current state
+						|| matchCore(string.substring(1), pattern)
+						// ignore a '*'
+						|| matchCore(string, pattern.substring(2));
+			else
+				// ignore a '*'
+				return matchCore(string, pattern.substring(2));
+		}
+		if (string.charAt(0) == pattern.charAt(0)
+				|| (pattern.charAt(0) == '.' && string.length()!=0))
+			return matchCore(string.substring(1), pattern.substring(1));
+		return false;
+	}
 
 	public static void main(String[] args) {
-		 
+		System.out.println(matchRegexp("aaa", "a*a"));
+		System.out.println(matchRegexp("ab", "a*a*c"));
+		System.out.println(matchRegexp("aba", "a*a*c"));
 	}
 
 }
