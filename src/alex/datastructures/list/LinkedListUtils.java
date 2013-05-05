47b04e614957b852a20c923953332d4af107babc
Alex Soares <alex.soares@gmail.com>
2013-05-05 02:20:53 -0300

    reversing a linked list

--- src/alex/datastructures/list/LinkedListUtils.java
+++ src/alex/datastructures/list/LinkedListUtils.java
@@ -157,6 +157,22 @@ public class LinkedListUtils {
 		return kth;
 	}
 
+	public static ListNode reverse(ListNode head) {
+		ListNode reversedHead = null;
+		ListNode node = head;
+		ListNode prev = null;
+		while (node != null) {
+			ListNode next = node.next;
+			if (next == null) {
+				reversedHead = node;
+			}
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return reversedHead;
+	}
+
 	public static void main(String[] args) {
 
 	}
