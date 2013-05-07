ab6384273926954bf917b47bef11c73ed8ecd0fd
Alex Soares <alex.soares@moip.com.br>
2013-05-07 07:41:14 -0300

    Distance between two nodes in a Tree

--- src/alex/algorithms/graphs/trees/binary/Node.java
+++ src/alex/algorithms/graphs/trees/binary/Node.java
@@ -3,63 +3,72 @@ package alex.algorithms.graphs.trees.binary;
 import java.util.LinkedList;
 
 public class Node<T extends Comparable<T>> {
-	private Node<T> left;
-	private Node<T> right;
-	private T value;
-	private LinkedList<Node<T>> adj = new LinkedList<>();
-	int baconNumber =-1;
+    private Node<T> left;
+    private Node<T> right;
+    private Node<T> parent;
+    private T value;
+    private LinkedList<Node<T>> adj = new LinkedList<>();
+    int baconNumber = -1;
 
-	public Node(T v) {
-		this.value = v;
-		this.left=null;
-		this.right = null;
-	}
-	
-	public Node<T> rightRotate(){
-		Node<T> root = this.left;
-		this.left = root.right;
-		root.right = this;
-		return root;
-	}
+    public Node(final T v) {
+        this.value = v;
+        this.left = null;
+        this.right = null;
+    }
 
-	public Node<T> getLeft() {
-		return left;
-	}
+    public Node<T> rightRotate() {
+        Node<T> root = this.left;
+        this.left = root.right;
+        root.right = this;
+        return root;
+    }
 
-	public void setLeft(Node<T> left) {
-		this.left = left;
-	}
+    public Node<T> getLeft() {
+        return left;
+    }
 
-	public Node<T> getRight() {
-		return right;
-	}
+    public void setLeft(final Node<T> left) {
+        this.left = left;
+    }
 
-	public void setRight(Node<T> right) {
-		this.right = right;
-	}
+    public Node<T> getRight() {
+        return right;
+    }
 
-	public T getValue() {
-		return value;
-	}
+    public void setRight(final Node<T> right) {
+        this.right = right;
+    }
 
-	public void setValue(T value) {
-		this.value = value;
-	}
+    public T getValue() {
+        return value;
+    }
 
-	public int getBaconNumber() {
-		return baconNumber;
-	}
+    public void setValue(final T value) {
+        this.value = value;
+    }
 
-	public void setBaconNumber(int baconNumber) {
-		this.baconNumber = baconNumber;
-	}
+    public int getBaconNumber() {
+        return baconNumber;
+    }
 
-	public LinkedList<Node<T>> getAdj() {
-		return adj;
-	}
+    public void setBaconNumber(final int baconNumber) {
+        this.baconNumber = baconNumber;
+    }
 
-	public void setAdj(LinkedList<Node<T>> adj) {
-		this.adj = adj;
-	}
+    public LinkedList<Node<T>> getAdj() {
+        return adj;
+    }
+
+    public void setAdj(final LinkedList<Node<T>> adj) {
+        this.adj = adj;
+    }
+
+    public Node<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(final Node<T> parent) {
+        this.parent = parent;
+    }
 
 }
