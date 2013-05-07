ab319221dbd135c4872294bfa16106c0b29e5fb4
Alex Soares <alex.soares@moip.com.br>
2013-05-07 07:40:31 -0300

    Refactoring

--- src/alex/algorithms/arrays/CountInversions.java
+++ src/alex/algorithms/arrays/CountInversions.java
@@ -20,7 +20,29 @@ public class CountInversions {
 		int left[] = Arrays.copyOfRange(arr, 0, m);
 		int right[] = Arrays.copyOfRange(arr, m, arr.length);
 	
-		return countInversions(left) + countInversions(right) + ArraysProblems.merge(arr, left, right);
+		return countInversions(left) + countInversions(right) + CountInversions.merge(arr, left, right);
 	}
+
+    public static int merge(int[] arr, int[] left, int[] right) {
+    	int i = 0, j = 0, count = 0;
+    	while (i < left.length || j < right.length) {
+    		if (i == left.length) {
+    			arr[i + j] = right[j];
+    			j++;
+    		} else if (j == right.length) {
+    			arr[i + j] = left[i];
+    			i++;
+    		} else if (left[i] <= right[j]) {
+    			arr[i + j] = left[i];
+    			i++;
+    		} else {
+    			arr[i + j] = right[j];
+    			// right element is lesser than all the rest of array left
+    			count += left.length - i;
+    			j++;
+    		}
+    	}
+    	return count;
+    }
 
 }
