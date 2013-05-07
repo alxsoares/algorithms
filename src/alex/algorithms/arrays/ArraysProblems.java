7c254f9127e870f48a1c9ddbdc4abd34be790820
Alex Soares <alex.soares@moip.com.br>
2013-05-07 07:40:10 -0300

    Refactoring

--- src/alex/algorithms/arrays/ArraysProblems.java
+++ src/alex/algorithms/arrays/ArraysProblems.java
@@ -390,28 +390,6 @@ public class ArraysProblems {
 		return false;
 	}
 
-	public static int merge(int[] arr, int[] left, int[] right) {
-		int i = 0, j = 0, count = 0;
-		while (i < left.length || j < right.length) {
-			if (i == left.length) {
-				arr[i + j] = right[j];
-				j++;
-			} else if (j == right.length) {
-				arr[i + j] = left[i];
-				i++;
-			} else if (left[i] <= right[j]) {
-				arr[i + j] = left[i];
-				i++;
-			} else {
-				arr[i + j] = right[j];
-				// right element is lesser than all the rest of array left
-				count += left.length - i;
-				j++;
-			}
-		}
-		return count;
-	}
-
 	/**
 	 * @param args
 	 */
