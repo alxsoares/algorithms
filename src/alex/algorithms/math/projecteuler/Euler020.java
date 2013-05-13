d70a114675f5f0a8bbffb70260c13cb6bd450ff4
Alex Soares <alex.soares@gmail.com>
2013-05-12 21:41:42 -0300

    Project Euler 20

---
+++ src/alex/algorithms/math/projecteuler/Euler020.java
@@ -0,0 +1,20 @@
+package alex.algorithms.math.projecteuler;
+
+import java.math.BigInteger;
+
+public class Euler020 {
+
+	public static void main(String[] args) {
+		BigInteger f = new BigInteger("100");
+		for(int i=99;i>=2;i--){
+			f = f.multiply(BigInteger.valueOf(i));
+		}
+		String s = f.toString();
+		BigInteger sum = BigInteger.valueOf(0);
+		for(int i=0; i<s.length();i++){
+			sum = sum.add(new BigInteger(s.substring(i, i+1)));
+		}
+		System.out.printf("%s\n",sum.toString());
+	}
+
+}
