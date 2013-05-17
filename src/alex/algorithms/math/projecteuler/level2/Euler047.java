862e14add41da13f49470865b003068a2c4540f8
Alex Soares <alex.soares@gmail.com>
2013-05-17 09:19:34 -0300

    Euler project 47

---
+++ src/alex/algorithms/math/projecteuler/level2/Euler047.java
@@ -0,0 +1,42 @@
+package alex.algorithms.math.projecteuler.level2;
+
+import java.util.Set;
+
+import alex.algorithms.math.projecteuler.ErastotenesSieve;
+
+public class Euler047 {
+	public static int numberOfPrimeFactors(int n, Set<Integer> primes) {
+		int res = 0;
+		for (Integer prime : primes) {
+			if (prime * prime > n) {
+				return ++res;
+			}
+			boolean isPrimeFactor = false;
+			while (n % prime == 0) {
+				isPrimeFactor = true;
+				n = n / prime;
+			}
+			if (isPrimeFactor)
+				res++;
+			if (n == 1)
+				return res;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		Set<Integer> sieve = ErastotenesSieve.sieve(10000);
+		int res = 2 * 3 * 5 * 7;
+		int consecutives = 1;
+		while (consecutives < 4) {
+			res++;
+			if (numberOfPrimeFactors(res, sieve) >= 4) {
+				consecutives++;
+			} else {
+				consecutives = 0;
+			}
+		}
+		System.out.printf("%d\n", res-3);
+	}
+
+}
