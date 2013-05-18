2648000af2b857b953d54c3614414b760fc0c11f
Alex Soares <alex.soares@gmail.com>
2013-05-18 19:38:27 -0300

    Project Euler 70

---
+++ src/alex/algorithms/math/projecteuler/level3/Euler070.java
@@ -0,0 +1,50 @@
+package alex.algorithms.math.projecteuler.level3;
+
+import alex.algorithms.math.projecteuler.ErastotenesSieve;
+
+public class Euler070 {
+	static boolean isPerm(long m, long n) {
+		int[] arr = new int[10];
+
+		long temp = n;
+		while (temp > 0) {
+			arr[(int) (temp % 10)]++;
+			temp /= 10;
+		}
+
+		temp = m;
+		while (temp > 0) {
+			arr[(int) (temp % 10)]--;
+			temp /= 10;
+		}
+
+		for (int i = 0; i < 10; i++) {
+			if (arr[i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Integer[] primes = ErastotenesSieve.sieve(2000, 5000);
+		long sol=1;
+		long solPhi=1;
+		double phiRatio =Double.POSITIVE_INFINITY;
+		for(int i=0; i<primes.length;i++){
+			for(int j=i+1;j<primes.length;j++){
+				long n = primes[i]*primes[j];
+				if(n > 10000000) break;
+				long phi = (primes[i]-1)*(primes[j]-1);
+				double ratio = ((double) n)/phi;
+				if(isPerm(n, phi) && ratio < phiRatio){
+					sol = n;
+					phiRatio = ratio;
+					solPhi = phi;
+				}
+			}
+		}
+		System.out.printf("%d %f %d\n", sol,phiRatio, solPhi);
+	}
+
+}
