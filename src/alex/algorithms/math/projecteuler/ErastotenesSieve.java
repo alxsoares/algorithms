f03653f5000a91bcec87073d66ecbabd3fe09b23
Alex Soares <alex.soares@gmail.com>
2013-05-18 19:20:05 -0300

    Project Euler 69

--- src/alex/algorithms/math/projecteuler/ErastotenesSieve.java
+++ src/alex/algorithms/math/projecteuler/ErastotenesSieve.java
@@ -1,6 +1,8 @@
 package alex.algorithms.math.projecteuler;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -26,4 +28,25 @@ public class ErastotenesSieve {
 		return primes;
 	}
 
+	public static Integer[] sieve(int lowerLimit, int upperLimit) {
+		List<Integer> numbers = new ArrayList<Integer>(
+				(int) (upperLimit / (Math.log(upperLimit) - 1.08366)));
+
+		BitSet bprimes = new BitSet(upperLimit + 1);
+		int sqrt = (int) Math.sqrt(upperLimit);
+		for (int i = 2; i <= sqrt; i++) {
+			if (!bprimes.get(i)) {
+				for (int j = 2 * i; j <= upperLimit; j += i) {
+					bprimes.set(j);
+				}
+			}
+
+		}
+		for (int i = lowerLimit; i <= upperLimit; i++) {
+			if (!bprimes.get(i)) {
+				numbers.add(i);
+			}
+		}
+		return numbers.toArray(new Integer[0]);
+	}
 }
---
+++ src/alex/algorithms/math/projecteuler/level3/Euler069.java
@@ -0,0 +1,18 @@
+package alex.algorithms.math.projecteuler.level3;
+
+import alex.algorithms.math.projecteuler.ErastotenesSieve;
+
+public class Euler069 {
+
+	public static void main(String[] args) {
+		int result = 1;
+		Integer[] primes = ErastotenesSieve.sieve(2, 200);
+		int i = 0;
+		while(result* primes[i] < 1000000){
+		    result *= primes[i];
+		    i++;
+		}
+		System.out.printf("%d\n", result);
+	}
+
+}
