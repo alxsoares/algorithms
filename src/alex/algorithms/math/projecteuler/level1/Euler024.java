5866190a37b4a2ec0cce51ca5da8636df4ff3309
Alex Soares <alex.soares@gmail.com>
2013-06-03 09:31:56 -0300

    refactoring

--- src/alex/algorithms/math/projecteuler/level1/Euler024.java
+++ src/alex/algorithms/math/projecteuler/level1/Euler024.java
@@ -18,13 +18,13 @@ public class Euler024 {
 		// find the digits of the factorial number n=999999
 		// to do that, we look how many times a factor i! is in n, starting at
 		// 9!
-		int[] fac_digits = new int[10];
+		int[] facDigits = new int[10];
 		for (int i = 10 - 1, n = 999999; i >= 0; i--) {
 			int remain = n % base[10 - 1 - i];
-			fac_digits[10 - 1 - i] = (n - remain) / base[10 - 1 - i];
+			facDigits[10 - 1 - i] = (n - remain) / base[10 - 1 - i];
 			n = remain;
 		}
-		System.out.println("F(999999): " + Arrays.toString(fac_digits));
+		System.out.println("F(999999): " + Arrays.toString(facDigits));
 
 		// fill an array with the permutation numbers 0...9
 		List<Integer> numbers = new ArrayList<Integer>(10);
@@ -34,11 +34,13 @@ public class Euler024 {
 
 		StringBuilder permutation = new StringBuilder();
 		for (int i = 0; i < 10; i++) {
-			permutation.append(numbers.remove(fac_digits[i]));
+			permutation.append(numbers.remove(facDigits[i]));
 		}
 		System.out.println("The 10^6 permutation is " + permutation);
 
 	}
+	
+	
 
 	public static int factorial(int n) {
 		if (n == 0)
