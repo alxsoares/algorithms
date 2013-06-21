ad3aa9f483a73d41d6fc9023495e7144480b0d02
Alex Soares <alex.soares@gmail.com>
2013-06-21 13:12:13 -0300

    Euler 427

--- src/alex/algorithms/math/projecteuler/Euler427a.java
+++ src/alex/algorithms/math/projecteuler/Euler427a.java
@@ -4,11 +4,11 @@ public class Euler427a {
     static final int p = 1000000009;
     static final double pinv = 1.0 / p;
 
-    static long MMod(final long a, final long b) {
+    static long multiMod(final long a, final long b) {
         return (a % p) * (b % p) % p;
     }
 
-    static long modpow(long b, long e, final long m) {
+    static long modPow(long b, long e, final long m) {
         long r = 1;
         while (e > 0) {
             if ((e & 1) != 0)
@@ -26,26 +26,24 @@ public class Euler427a {
         int n = 7500000;
         int n1 = n - 1;
         int p2 = p - 2;
-        long invN = modpow(n, p2, p);
+        long invN = modPow(n, p2, p);
 
         // build the factorial and power cache for fast binomial coeff.
         // calculation
-        long cacheFact[], cacheInvFact[], cachePowN1[], cachePowN[], cacheInvPowN[];
-
-        cacheFact = new long[n + 1];
-        cacheInvFact = new long[n + 1];
-        cachePowN1 = new long[n + 1];
-        cachePowN = new long[n + 1];
-        cacheInvPowN = new long[n + 1];
+        long [] cacheFact = new long[n + 1];
+        long [] cacheInvFact = new long[n + 1];
+        long [] cachePowN1 = new long[n + 1];
+        long [] cachePowN = new long[n + 1];
+        long [] cacheInvPowN = new long[n + 1];
 
         cacheFact[0] = cacheInvFact[0] = cachePowN1[0] = cachePowN[0] = cacheInvPowN[0] = 1;
 
         for (int i = 1; i <= n; i++) {
-            cacheFact[i] = MMod(cacheFact[i - 1], i);
-            cacheInvFact[i] = modpow(cacheFact[i], p2, p);
-            cachePowN1[i] = MMod(cachePowN1[i - 1], n1);
-            cachePowN[i] = MMod(cachePowN[i - 1], n);
-            cacheInvPowN[i] = MMod(cacheInvPowN[i - 1], invN);
+            cacheFact[i] = multiMod(cacheFact[i - 1], i);
+            cacheInvFact[i] = modPow(cacheFact[i], p2, p);
+            cachePowN1[i] = multiMod(cachePowN1[i - 1], n1);
+            cachePowN[i] = multiMod(cachePowN[i - 1], n);
+            cacheInvPowN[i] = multiMod(cacheInvPowN[i - 1], invN);
         }
         System.out.println("built caches, starting...");
 
@@ -65,43 +63,43 @@ public class Euler427a {
 
                 if (nmj2 >= j) {
                     long npow1 = nmj0 >= 0 ? cachePowN[nmj0] : cacheInvPowN[-nmj0];
-                    long comb1 = MMod(cacheFact[nmj2], MMod(cacheInvFact[j], cacheInvFact[nmj1]));
+                    long comb1 = multiMod(cacheFact[nmj2], multiMod(cacheInvFact[j], cacheInvFact[nmj1]));
 
-                    first = MMod(MMod(cachePowN1[j], npow1), comb1);
+                    first = multiMod(multiMod(cachePowN1[j], npow1), comb1);
                 }
 
                 if (j > 0) {
                     int exp2 = nmj0 + 1;
                     long npow2 = exp2 >= 0 ? cachePowN[exp2] : cacheInvPowN[-exp2];
 
-                    long comb2 = MMod(cacheFact[nmj2], MMod(cacheInvFact[j - 1], cacheInvFact[nmj0]));
+                    long comb2 = multiMod(cacheFact[nmj2], multiMod(cacheInvFact[j - 1], cacheInvFact[nmj0]));
 
-                    second = MMod(MMod(cachePowN1[j - 1], npow2), comb2);
+                    second = multiMod(multiMod(cachePowN1[j - 1], npow2), comb2);
                 }
 
-                long total = AddMod(first, second, p);
+                long total = addMod(first, second, p);
 
                 // multiply by (-1) ^ j
                 if (j % 2 == 1)
                     total = p - total;
 
-                result = AddMod(result, total, p);
+                result = addMod(result, total, p);
             }
             System.out.println("Result=" + result);
         }
 
         // result = n^(n+1)-result
-        long nn = MMod(cachePowN[n], n);
-        result = SubMod(nn, result, p);
+        long nn = multiMod(cachePowN[n], n);
+        result = subMod(nn, result, p);
 
         System.out.println("Result=" + result);
     }
 
-    private static long SubMod(final long first, final long second, final int p2) {
+    private static long subMod(final long first, final long second, final int p2) {
         return ((first % p2) - (second % p2)) % p2;
     }
 
-    private static long AddMod(final long first, final long second, final int p2) {
+    private static long addMod(final long first, final long second, final int p2) {
 
         return ((first % p2) + (second % p2)) % p2;
     }
